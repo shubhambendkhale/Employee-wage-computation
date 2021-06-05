4fc49cfd9e8135452fbe64b56e0fe17c7381b86d
shubhambendkhale <shubhambendkhale90@gmail.com>
2021-06-05 22:22:25 +0530

    Added: EmpDailyWage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,8 +1,14 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 random=$((RANDOM%2))
+wagePerHour=20
+workingHr=0
 if [ $random -eq 1 ]
 then
 	echo "Employee present"
+	workingHr=8
 else
 	echo "Employee absent"
+	workingHr=0
 fi
+salary=$((wagePerHour*workingHr))
+echo "Daily employee wage : $salary"
