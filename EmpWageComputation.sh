f93a2d9ed1ca99c2b51a50d08ab79360e21bdd0f
shubhambendkhale <shubhambendkhale90@gmail.com>
2021-06-05 22:34:22 +0530

    Added: PartTimeEmployeeAndWage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,11 +1,15 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
-random=$((RANDOM%2))
+random=$((RANDOM%3))
 wagePerHour=20
 workingHr=0
 if [ $random -eq 1 ]
 then
-	echo "Employee present"
+	echo "Employee present for Full time"
 	workingHr=8
+elif [ $random -eq 2 ]
+then
+	echo "Employee present for Part time"
+	workingHr=4
 else
 	echo "Employee absent"
 	workingHr=0
