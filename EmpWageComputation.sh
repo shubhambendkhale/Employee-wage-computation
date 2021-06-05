774d9743067dd3e129bb36332ef7852a3ecc0653
shubhambendkhale <shubhambendkhale90@gmail.com>
2021-06-05 22:42:33 +0530

    Added: EmpDailyWageUsingCase

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,17 +2,19 @@ echo "Welcome to Employee Wage Computation Program on Master Branch"
 random=$((RANDOM%3))
 wagePerHour=20
 workingHr=0
-if [ $random -eq 1 ]
-then
-	echo "Employee present for Full time"
-	workingHr=8
-elif [ $random -eq 2 ]
-then
-	echo "Employee present for Part time"
-	workingHr=4
-else
-	echo "Employee absent"
-	workingHr=0
-fi
+case $random in
+	1)
+		echo "Employee present for Full time"
+		workingHr=8
+		;;
+	2)
+		echo "Employee present for Part time"
+		workingHr=4
+		;;
+	*)
+		echo "Employee Absent"
+		workingHr=0
+		;;
+esac
 salary=$((wagePerHour*workingHr))
 echo "Daily employee wage : $salary"
