a65e833d00dd4c4dc1c3d8abea027fb1bf85d1ea
shubhambendkhale <shubhambendkhale90@gmail.com>
2021-06-05 23:06:35 +0530

    Added: RefactorUsingFunction

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,14 +1,11 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
-
 wagePerHour=20
 workingDays=20
 maxHrsInMonth=100
 monthSalary=0
 totalWorkingHr=0
-for (( i=0; i<$workingDays && $totalWorkingHr<$maxHrsInMonth; i++ ))
-do
-	
-	random=$((RANDOM%3))
+
+function getWorkingHours() {
 	case $random in
 		1)
 			workingHr=8
@@ -20,6 +17,14 @@ do
 			workingHr=0
 			;;
 	esac
+	echo $workingHr
+}
+
+for (( i=0; i<$workingDays && $totalWorkingHr<$maxHrsInMonth; i++ ))
+do
+	
+	random=$((RANDOM%3))
+	workingHr="$( getWorkingHours $random )"
 	totalWorkingHr=$((totalWorkingHr+workingHr))
 done
 monthSalary=$((wagePerHour*totalWorkingHr))
